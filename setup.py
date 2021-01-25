0c301321cb120ed755da967d2a8b7774cea56268
lukew3 <lukew25073@gmail.com>
2021-01-24 20:50:48 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vid2slides',
+    version='0.0.1',
+    description='Extract a slideshow from a video presentation',
+    url='https://github.com/lukew3/vid2slides',
+    author='Luke Weiler',
+    author_email='lukew25073@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['click'],
+    entry_points={
+        'console_scripts': ['vid2slides=vid2slides.cli:cli'],
+    },
+)
---
+++ vid2slides/cli.py
@@ -0,0 +1,7 @@
+import click
+
+@click.command()
+def cli():
+    """ Extracts a slideshow from a video presentation """
+
+

4a0db3288b0e490be5118689038b32e2e33e1797
lukew3 <lukew25073@gmail.com>
2021-01-31 15:50:07 -0500

    Added missing v2s.py file

---
+++ vid2slides/v2s.py
@@ -0,0 +1,55 @@
+import os
+import shutil
+from PIL import Image, ImageChops, ImageStat
+
+
+def extract_slides(video_path, frames_folder):
+    # Fill a folder with all of the images
+    width = 1280
+    height = 720
+    if not os.path.exists(frames_folder):
+        os.makedirs(frames_folder)
+    #should I add an else statement here to delete frames folder if it exists?
+    # Is -s WxH necessary?
+    script = f'ffmpeg -loglevel quiet -i {video_path} -vsync 0 -vf select="eq(pict_type\,PICT_TYPE_I)" -f image2 frames/foo-%03d.jpeg'
+    print("Extracting frames...")
+    os.system(script)
+    remove_duplicates(frames_folder)
+
+
+def remove_duplicates(frames_folder):
+    """ Goes through each file in the passed folder and removes if difference ratio is less than 2.0"""
+    print("Removing duplicates...")
+    filename_list = []
+    for filename in os.listdir(frames_folder):
+        filename_list.append(filename)
+    filename_list.sort()
+    last_frame = Image.open(frames_folder + '/' + filename_list[0])
+    duplicates_list = []
+    for i in range(1, len(filename_list)):
+        cur_frame = Image.open(frames_folder + '/' + filename_list[i])
+        diff_img = ImageChops.difference(cur_frame, last_frame)
+        stat = ImageStat.Stat(diff_img)
+        diff_ratio = 100 * (sum(stat.mean) / (len(stat.mean) * 255))
+        # print(filename_list[i] + ": " + str(diff_ratio))
+        # If the images are identical, remove the previous file
+        # 2.0 is what worked for me, might want to tweak this with an option
+        if diff_ratio <= 2.0:
+            duplicates_list.append(frames_folder + '/' + filename_list[i-1])
+        last_frame = cur_frame
+    #remove each image in list
+    for image in duplicates_list:
+        os.remove(image)
+
+
+def write_to_pdf(frames_folder, output="out.pdf"):
+    frames_list = []
+    images = []
+    for filename in os.listdir(frames_folder):
+        frames_list.append(filename)
+    frames_list.sort()
+    for frame in frames_list:
+        images.append(Image.open(frames_folder + '/' + frame).convert("RGB"))
+    images[0].save(output, save_all=True, append_images=images[1:])
+    print("Output written to " + output)
+
